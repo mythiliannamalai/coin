deadea2ac16ed87cec9cbee8ee36bb50670f75c3
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-05 20:09:50 +0530

    head_tail

---
+++ coin_h_t/Program.cs
@@ -0,0 +1,11 @@
+﻿int coin_head = 1;
+Random random = new Random();
+int Coincheck = random.Next(0, 2);
+if (Coincheck == coin_head)
+{
+    Console.WriteLine("coin is head");
+}
+else
+{
+    Console.WriteLine("coin is tails");
+}
